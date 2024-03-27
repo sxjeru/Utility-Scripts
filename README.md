86642a2c30cd94b020818764ed15a1c6f500e6b2
sxjeru <sxjeru@gmail.com>
2024-03-27 15:51:40 +0800

    SR updater

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
+### 星穹铁道 更新补丁应用
+适用于 hdiff 补丁包，免于下载日渐庞大的完整游戏。
+
 ### [OpenAI Cost 统计](https://github.com/sxjeru/Utility-Scripts/tree/main/openai-apikey-cost)
 适用于无账号登录权限，只有 apikey 的情况下，简单统计某个 key 在某一时段内的总费用。
 
-### [mc百科文件自动更新](https://github.com/sxjeru/Utility-Scripts/tree/main/mcmod-auto-upload) (Working ~)
+### [mc百科文件 批量更新](https://github.com/sxjeru/Utility-Scripts/tree/main/mcmod-auto-upload)
 目前已实现半自动更新 CurseForge 每日邮件中提到的 mod。
 
 此外还包含 mc百科的文件上传API，可供希望在百科自动化更新自己 mod 的创作者参考。
---
+++ StarRail-hdiff-update/StarRail.py
@@ -0,0 +1,34 @@
+# 1. 解压 hdiff 并覆盖游戏根目录
+# 2. 确保本脚本与 hpatchz.exe 同在游戏根目录
+import os, re, glob, json, hashlib, time
+start = time.time()
+
+with open("deletefiles.txt", 'r') as f:
+    for line in f.readlines():
+        os.remove(line.strip())
+print("Delete done! ({:.2f}s)\n".format(time.time() - start))
+os.remove("deletefiles.txt")
+
+hd = []
+with open("hdifffiles.txt", 'r') as f:
+    for line in f.readlines():
+        hd.append(re.findall(r'"(.*?)"', line)[1])
+for i in hd:
+    os.system("hpatchz.exe -f " + i + " " + i + ".hdiff " + i)
+    os.remove(i + ".diff")
+print("\nPatch done! ({:.2f}s)\n".format(time.time() - start))
+os.remove("hdifffiles.txt")
+
+hdpath = os.path.dirname(hd[0])
+md5txt = glob.glob(os.path.join(hdpath, 'AudioV_*'))
+with open(md5txt[0], 'r') as f:
+    for line in f.readlines():
+        path = os.path.join(hdpath, json.loads(line)['Path'])
+        with open(path, 'rb') as f:
+            md5 = hashlib.md5(f.read()).hexdigest()
+        if md5 != json.loads(line)['Md5']:
+            print("有文件校验失败：" + path + "\n\n程序退出...")
+            exit(1)
+print("All files are correct. Update done! ({:.2f}s)\n".format(time.time() - start))
+import msvcrt
+msvcrt.getch()
